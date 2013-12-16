91f1d92c7d537c839ea6fb6f3612a0a28f7e8b20
marc <marc@pingfu.net>
2013-12-16 21:32:34 +0000

    restructured base58 encoder

--- blockchainsharp/blockchainsharp/BlockChainReader.cs
+++ blockchainsharp/blockchainsharp/BlockChainReader.cs
@@ -6,6 +6,7 @@ using System.Security.Cryptography;
 using BlockChainSharp.Dto;
 using BlockChainSharp.PInvoke;
 using BlockChainSharp.Util;
+using BlockChainSharp.Util.Encoding;
 
 namespace BlockChainSharp
 {
